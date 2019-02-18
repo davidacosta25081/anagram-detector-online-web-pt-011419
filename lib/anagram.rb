8d9e1e2c9be45f11802bb0d5e3ec1b1dbb18f7e9
David Acosta <davidacosta25081@yahoo.com>
2019-02-18 23:11:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ attr_accessor :name
   end
 
   def match(array)
-    array.collect {|x| x.split("").sort== @name.split("").sort}
+    array.select {|x| x.split("").sort== @name.split("").sort}
   end
 
 end
